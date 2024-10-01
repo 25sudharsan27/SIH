8eb120533947ae923785ac193b401479bbda4faf
25sudharsan27 <25sudharsan27@gmail.com>
2024-10-01 09:32:48 +0530

    updated cors

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,8 @@ const app = express();
 
 
 app.use(cors({
-    origin : process.env.FRONTEND_URL,
+    origin : [process.env.FRONTEND_URL,'http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials : true
 }
 )) ;
