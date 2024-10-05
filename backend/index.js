271c9366cb1bb42c68a20cb73a050e1894c1a1ed
25sudharsan27 <25sudharsan27@gmail.com>
2024-10-05 18:01:48 +0530

    replaced the index js of the backend

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 
 app.use(cors({
-    origin : process.env.FRONTEND_URL,
+    origin : "https://frontend-sih-swart.vercel.app",
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials : true
 }
