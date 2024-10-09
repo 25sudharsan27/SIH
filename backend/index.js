e68b136f41ab1b68c51fb77dacc7b98eeac9548e
25sudharsan27 <25sudharsan27@gmail.com>
2024-10-10 00:46:03 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 
 app.use(cors({
-    origin : "https://frontend-sih-swart.vercel.app","http://localhost:3000",
+    origin : "https://frontend-sih-swart.vercel.app",
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials : true
 }
