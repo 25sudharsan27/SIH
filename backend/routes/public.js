9cd66b29b7ce6d7a97dc1b097fc74c9666979945
25sudharsan27 <25sudharsan27@gmail.com>
2025-05-21 23:01:32 +0530

    changes done

--- backend/routes/public.js
+++ backend/routes/public.js
@@ -3,7 +3,7 @@ const express = require("express")
 const router = express.Router();
 
 const authToken = require("../middleware/verifyuserToken");
-const jobdetails = require("../controller/public/jobDetails");
+const jobdetails = require("../controller/public/jobDetails.js");
 const updatejob = require("../controller/public/updateJob");
 const filterjobdetails = require("../controller/public/filterJobDetails");
 const viewJob = require("../controller/public/singleJobDetails");
